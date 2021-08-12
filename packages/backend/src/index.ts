2de0c8e66c06381dcf0f982ec624d977972819eb
Mike Lewis <mtlewis@users.noreply.github.com>
2021-08-12 11:25:58 +0100

    example-backend: incorporate permission backend
    Signed-off-by: Mike Lewis <mtlewis@users.noreply.github.com>
    Co-authored-by: Fredrik Adelöw <freben@gmail.com>
    Co-authored-by: Himanshu Mishra <himanshu@orkohunter.net>
    Co-authored-by: Johan Haals <johan.haals@gmail.com>
    Co-authored-by: Patrik Oldsberg <poldsberg@gmail.com>

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -43,6 +43,7 @@ import kubernetes from './plugins/kubernetes';
 import kafka from './plugins/kafka';
 import rollbar from './plugins/rollbar';
 import scaffolder from './plugins/scaffolder';
+import permission from './plugins/permission';
 import proxy from './plugins/proxy';
 import search from './plugins/search';
 import techdocs from './plugins/techdocs';
@@ -92,6 +93,7 @@ async function main() {
   );
   const scaffolderEnv = useHotMemoize(module, () => createEnv('scaffolder'));
   const authEnv = useHotMemoize(module, () => createEnv('auth'));
+  const permissionEnv = useHotMemoize(module, () => createEnv('permission'));
   const proxyEnv = useHotMemoize(module, () => createEnv('proxy'));
   const rollbarEnv = useHotMemoize(module, () => createEnv('rollbar'));
   const searchEnv = useHotMemoize(module, () => createEnv('search'));
@@ -115,6 +117,7 @@ async function main() {
   apiRouter.use('/todo', await todo(todoEnv));
   apiRouter.use('/kubernetes', await kubernetes(kubernetesEnv));
   apiRouter.use('/kafka', await kafka(kafkaEnv));
+  apiRouter.use('/permission', await permission(permissionEnv));
   apiRouter.use('/proxy', await proxy(proxyEnv));
   apiRouter.use('/graphql', await graphql(graphqlEnv));
   apiRouter.use('/badges', await badges(badgesEnv));
