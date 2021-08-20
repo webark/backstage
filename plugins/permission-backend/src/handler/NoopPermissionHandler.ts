416914933c2f4d47251a5a1313bdf8e1d2638376
Mike Lewis <mtlewis@users.noreply.github.com>
2021-08-20 13:05:24 +0100

    Add @backstage/plugin-permission-backend
    Adds a pluggable permission backend along with a default no-op handler.
    
    See the branch mob/authorization framework for the full
    proof-of-concept.
    
    Signed-off-by: Mike Lewis <mtlewis@users.noreply.github.com>
    Co-authored-by: Fredrik Adelöw <freben@gmail.com>
    Co-authored-by: Himanshu Mishra <himanshu@orkohunter.net>
    Co-authored-by: Joe Porpeglia <porpegliaj@gmail.com>
    Co-authored-by: Johan Haals <johan.haals@gmail.com>
    Co-authored-by: Patrik Oldsberg <poldsberg@gmail.com>
    Co-authored-by: Tim Hansen <timbonicus@gmail.com>

---
+++ plugins/permission-backend/src/handler/NoopPermissionHandler.ts
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2021 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { BackstageIdentity } from '@backstage/plugin-auth-backend';
+import {
+  AuthorizeRequest,
+  AuthorizeRequestContext,
+  AuthorizeResponse,
+  AuthorizeResult,
+} from '@backstage/plugin-permission';
+import { PermissionHandler } from './types';
+
+// TODO(mtlewis/orkuhunter): Maybe we can find a clearer name for this?
+// "AllowAllPermissionHander"? "UnrestrictedPermissionHandler"?
+export class NoopPermissionHandler implements PermissionHandler {
+  async handle(
+    _request: AuthorizeRequest<AuthorizeRequestContext>,
+    _user?: BackstageIdentity,
+  ): Promise<AuthorizeResponse> {
+    return {
+      result: AuthorizeResult.ALLOW,
+    };
+  }
+}
---
+++ plugins/permission-backend/src/service/router.ts
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2020 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { errorHandler, SingleHostDiscovery } from '@backstage/backend-common';
+import express, { Request, Response } from 'express';
+import Router from 'express-promise-router';
+import { Logger } from 'winston';
+import { IdentityClient } from '@backstage/plugin-auth-backend';
+import { Config } from '@backstage/config';
+import {
+  AuthorizeRequestJSON,
+  AuthorizeResponse,
+  Permission,
+} from '@backstage/plugin-permission';
+import { NoopPermissionHandler, PermissionHandler } from '../handler';
+
+export interface RouterOptions {
+  env: {
+    logger: Logger;
+    config: Config;
+  };
+  permissionHandler?: PermissionHandler;
+}
+
+export async function createRouter(
+  options: RouterOptions,
+): Promise<express.Router> {
+  const {
+    env: { logger, config },
+    permissionHandler = new NoopPermissionHandler(),
+  } = options;
+  const discovery = SingleHostDiscovery.fromConfig(config);
+  const identity = new IdentityClient({
+    discovery,
+    issuer: await discovery.getExternalBaseUrl('auth'),
+  });
+
+  const router = Router();
+  router.use(express.json());
+
+  router.post(
+    '/authorize',
+    async (
+      req: Request<AuthorizeRequestJSON[]>,
+      res: Response<AuthorizeResponse[]>,
+    ) => {
+      // TODO(mtlewis/orkohunter): Payload too large errors happen when internal backends (techdocs, search, etc.) try
+      // to fetch all of entities.
+      // Fix1: The request should only contain entity refs instead of full entity.
+      // Fix2: We should probably not filter requests from other backends -
+      // either by allowing them a superuser token or treating their requests separately from a user's request.
+      const token = IdentityClient.getBearerToken(req.header('authorization'));
+
+      if (token) {
+        const user = await identity.authenticate(token);
+        logger.info(`authorizing as user: ${user.id}...`);
+
+        res.json(
+          await Promise.all(
+            req.body.map(({ permission, context }: AuthorizeRequestJSON) =>
+              permissionHandler.handle(
+                {
+                  permission: Permission.fromJSON(permission),
+                  context,
+                },
+                user,
+              ),
+            ),
+          ),
+        );
+      } else {
+        logger.info('authorizing anonymously...');
+
+        res.json(
+          await Promise.all(
+            req.body.map(({ permission, context }: AuthorizeRequestJSON) =>
+              permissionHandler.handle({
+                permission: Permission.fromJSON(permission),
+                context,
+              }),
+            ),
+          ),
+        );
+      }
+    },
+  );
+
+  router.use(errorHandler());
+  return router;
+}
